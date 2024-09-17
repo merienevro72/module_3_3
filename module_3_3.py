ac70884ac188ea7b7077d845a6051a6ab3d288b2
merienevro72 <merienevro72@yandex.ru>
2024-09-17 21:27:48 +0500

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print (a,b,c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [5, 'list', True]
+values_dict = {'a':1, 'b':False, 'c': [3,2,1]}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
